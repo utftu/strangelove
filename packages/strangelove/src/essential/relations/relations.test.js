42a0182f73a374207fe5d3057134de57e781840f
utftufutukgyftryidytftuv <overby999@gmail.com>
2022-09-29 02:26:07 +0300

    29.09.22 02:26

--- packages/strangelove/src/essential/relations/relations.test.js
+++ packages/strangelove/src/essential/relations/relations.test.js
@@ -25,23 +25,6 @@ describe('relations', () => {
     expect(parent.relations.children.size).toBe(0);
     expect(child.relations.parents.size).toBe(0);
   });
-
-  it('_addParent()', () => {
-    const child = new Atom();
-    const parent = new Atom();
-
-    child.relations._addParent(parent);
-    expect(child.relations.parents.size).toBe(1);
-    expect([...child.relations.parents][0]).toBe(parent);
-  });
-  it('_removeParent()', () => {
-    const child = new Atom();
-    const parent = new Atom();
-
-    child.relations._addParent(parent);
-    child.relations._removeParent(parent);
-    expect(child.relations.parents.size).toBe(0);
-  });
   it('replaceParents()', () => {
     const child = new Atom();
     const oldParent = new Atom();
@@ -51,22 +34,6 @@ describe('relations', () => {
     child.relations.replaceParents(new Set([newParent]));
     expect(child.relations.parents.size).toBe(1);
     expect([...child.relations.parents][0]).toBe(newParent);
-  });
-  it('_addChild()', () => {
-    const parent = new Atom();
-    const child = new Atom();
-
-    parent.relations._addChild(child);
-    expect(parent.relations.children.size).toBe(1);
-    expect([...parent.relations.children][0]).toBe(child);
-  });
-  it('_removeChild()', () => {
-    const parent = new Atom();
-    const child = new Atom();
-
-    parent.relations._addChild(child);
-    parent.relations._removeChild(child);
-    expect(parent.relations.children.size).toBe(0);
   });
   it('replaceChildren()', () => {
     const parent = new Atom();
