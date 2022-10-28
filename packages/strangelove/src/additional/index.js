d44515ce52b4af7b19da701fd3666941c3a334ae
utftufutukgyftryidytftuv <overby999@gmail.com>
2022-10-28 21:31:29 +0300

    28.10.22 21:31

--- packages/strangelove/src/additional/index.js
+++ packages/strangelove/src/additional/index.js
@@ -1,6 +1,33 @@
 import AtomSyncRoot from './atom/atom-sync-root.js';
 import AtomAsyncRoot from './atom/atom-async-root.js';
+import createAtomAsyncRoot from './root/create-atom-async-root.js';
+import createAtomSyncRoot from './root/create-atom-sync-root.js';
+import createStateAtomAsyncRoot from './root/create-state-atom-async-root.js';
+import createStateAtomSyncRoot from './root/create-state-atom-sync-root.js';
+import selectAsyncRoot from './select/select-async-root.js';
+import selectRoot from './select/select-root.js';
+import selectSyncRoot from './select/select-sync-root.js';
 
-export {};
+export {
+  AtomSyncRoot,
+  AtomAsyncRoot,
+  createAtomAsyncRoot,
+  createAtomSyncRoot,
+  createStateAtomSyncRoot,
+  createStateAtomAsyncRoot,
+  selectRoot,
+  selectSyncRoot,
+  selectAsyncRoot,
+};
 
-export default {};
+export default {
+  AtomSyncRoot,
+  AtomAsyncRoot,
+  createAtomAsyncRoot,
+  createAtomSyncRoot,
+  createStateAtomSyncRoot,
+  createStateAtomAsyncRoot,
+  selectRoot,
+  selectSyncRoot,
+  selectAsyncRoot,
+};
