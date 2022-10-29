e3e9bc02846d16df9356dedf23c119b5107630f0
utftufutukgyftryidytftuv <overby999@gmail.com>
2022-10-30 01:35:18 +0300

    30.10.22 01:35

--- packages/strangelove/types/essential/read-value-async.d.ts
+++ packages/strangelove/types/essential/read-value-async.d.ts
@@ -1,7 +1,7 @@
 export interface ReadValueAsyncConfig<TData> {
   get(): Promise<TData>;
-  needCheckPrev: boolean;
-  value: TData;
+  needCheckPrev?: boolean;
+  value?: TData;
 }
 
 export default class ReadValueAsync<TData> {
