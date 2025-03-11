ee0450c98bccce01e962bd3f1cc210e13596fb3e
utftufutukgyftryidytftuv <overby999@gmail.com>
2025-03-11 13:28:09 +0300

    11.03.25 13:28

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     target: "esnext",
-    outDir: `./dist`,
+    outDir: "./dist",
     lib: {
       entry: ["./src/strangelove.ts"],
       formats: ["es"],
     },
   },
-  plugins: [
-    dts({
-      outDir: "./dist/types",
-      tsconfigPath: "./tsconfig.types.json",
-    }),
-  ],
 });
